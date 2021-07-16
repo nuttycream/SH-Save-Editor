9c80888c2409352ace9351138606890c879c541d
nuttycream <issanutty@gmail.com>
2021-07-15 20:06:18 -0700

    Fixed ViewModel and DataContext

--- SpaceHaven Save Editor/Views/MainWindow.xaml.cs
+++ SpaceHaven Save Editor/Views/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using SpaceHaven_Save_Editor.ViewModels;
 
 namespace SpaceHaven_Save_Editor.Views
 {
@@ -10,6 +15,22 @@ namespace SpaceHaven_Save_Editor.Views
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void NumberTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^0-9]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            var sInfo = new ProcessStartInfo(e.Uri.ToString())
+            {
+                UseShellExecute = true,
+            };
+            Process.Start(sInfo);
         }
     }
 }
