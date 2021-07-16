99b3170744f0062094f508ff717cf2d180e580ca
nuttycream <issanutty@gmail.com>
2021-07-15 22:25:33 -0700

    Added ico

---
+++ SpaceHaven Save Editor/Views/About.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace SpaceHaven_Save_Editor.Views
+{
+    public partial class About : UserControl
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            var sInfo = new ProcessStartInfo(e.Uri.ToString())
+            {
+                UseShellExecute = true
+            };
+            Process.Start(sInfo);
+        }
+    }
+}
