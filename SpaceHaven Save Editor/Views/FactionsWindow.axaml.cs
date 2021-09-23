b834d02eea778edc0534c770309cf6e76b4fdec1
nuttycream <issanutty@gmail.com>
2021-09-22 22:35:23 -0700

    Added more screenies

---
+++ SpaceHaven Save Editor/Views/FactionsWindow.axaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using SpaceHaven_Save_Editor.Data;
+using SpaceHaven_Save_Editor.ViewModels;
+
+namespace SpaceHaven_Save_Editor.Views
+{
+    public class FactionsWindow : ReactiveWindow<FactionsViewModel>
+    {
+        public FactionsWindow(IEnumerable<Faction> interactionInput)
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            DataContext = new FactionsViewModel(interactionInput);
+            
+            this.WhenActivated(d => d(ViewModel!.SaveAndExit.Subscribe(Close)));
+        }
+        
+        public FactionsWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            DataContext = new FactionsViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
