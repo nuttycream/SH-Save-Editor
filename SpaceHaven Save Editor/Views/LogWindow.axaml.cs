9e3d74a4de643fc77cf7f27baa37d42a67c1e17a
nuttycream <issanutty@gmail.com>
2021-09-26 18:56:55 -0700

    Updated Screenie

---
+++ SpaceHaven Save Editor/Views/LogWindow.axaml.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using SpaceHaven_Save_Editor.ViewModels;
+
+namespace SpaceHaven_Save_Editor.Views
+{
+    public class LogWindow : ReactiveWindow<LogViewModel>
+    {
+        public LogWindow(string text, string logLocation)
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+            DataContext = new LogViewModel(text, logLocation);
+        }
+        
+        public LogWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
